70368048b4b5c0acb794983b241f93c886db52cc
Ciovino <tarantino.giovanbattista01@gmail.com>
2025-12-12 17:58:50 +0100

    add template file and update aoc-manager.py, v.1.1.3

---
+++ base-template.py
@@ -0,0 +1,72 @@
+# ---------------------------------------------------------------------
+# Advent of Code $year - Day $day_padded - $title_clean
+# Problem: See ./$desc_path for full details
+# Author: Ciovino
+# ---------------------------------------------------------------------
+import os
+import argparse
+import time
+
+# Useful imports
+import re
+from collections import defaultdict, Counter, deque
+from itertools import combinations, permutations, product
+from math import gcd, lcm, ceil, floor
+
+INPUT_FILE = os.path.join('$data_folder', '$input_name')
+TEST_FILE = os.path.join('$data_folder', 'test.in')
+VERBOSE = False
+
+def log(*args, **kwargs):
+    if VERBOSE: # Print only if VERBOSE is enabled
+        print(*args, **kwargs)
+
+def get_args() -> dict:
+    parser = argparse.ArgumentParser(description="Solution script for $day_padded/$year Advent of Code.")
+    parser.add_argument('-t', '--test', action='store_true',  help=f"Run the script using the test file ({TEST_FILE})")
+    parser.add_argument('-v', '--verbose', action='store_true', help="Enable verbose output.")
+    return parser.parse_args()
+
+def parse_input(file_name):
+    with open(file_name, 'r') as f:
+        data = f.read().splitlines()
+    return data
+
+# --- SOLVE ---
+def solve_part1(data):
+    """Solution for Part 1."""
+    return None
+
+def solve_part2(data):
+    """Solution for Part 2."""
+    return None
+
+if __name__ == '__main__':
+    args = get_args()
+    if args.test:
+        if not os.path.exists(TEST_FILE):
+            print(f"ERROR: Test file '{TEST_FILE}' not found.")
+            exit(1)
+        use_file = TEST_FILE
+    else:
+        use_file = INPUT_FILE
+    VERBOSE = args.verbose
+    
+    # Parsing
+    start_time = time.time()
+    data = parse_input(use_file)
+    log(f"Input parsed in {time.time()-start_time:.4f}s")
+    
+    # Part 1
+    start_time = time.time()
+    sol1 = solve_part1(data)
+    log(f"Part 1: {sol1}, took {time.time()-start_time:.4f}s")
+    
+    # Part 2
+    start_time = time.time()
+    sol2 = solve_part2(data)
+    log(f"Part 2: {sol2}, took {time.time()-start_time:.4f}s")
+
+    # --- PRINT SOLUTIONS ---
+    print(f"AOC_SOL_1={sol1}")
+    print(f"AOC_SOL_2={sol2}")
