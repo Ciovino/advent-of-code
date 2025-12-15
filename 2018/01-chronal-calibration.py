aae620cf0aad86a9ae33f7ab923dfc83139a7953
Ciovino <tarantino.giovanbattista01@gmail.com>
2025-12-15 23:35:38 +0100

    2018 - day01

---
+++ 2018/01-chronal-calibration.py
@@ -0,0 +1,82 @@
+# ---------------------------------------------------------------------
+# Advent of Code 2018 - Day 01 - Chronal Calibration
+# Problem: See ./2018/01-chronal-calibration-description.md for full details
+# Author: Ciovino
+# Template Version: v2.0
+# ---------------------------------------------------------------------
+import os
+import argparse
+import time
+
+# Useful imports
+import re
+from collections import defaultdict, Counter, deque
+from itertools import combinations, permutations, product, cycle
+from math import gcd, lcm, ceil, floor
+
+INPUT_FILE = os.path.join('data', '2018-01.in')
+TEST_FILE = os.path.join('data', 'test.in')
+VERBOSE = False
+
+def log(*args, **kwargs):
+    if VERBOSE: # Print only if VERBOSE is enabled
+        print(*args, **kwargs)
+
+def get_args() -> dict:
+    parser = argparse.ArgumentParser(description="Solution script for 01/2018 Advent of Code.")
+    parser.add_argument('-t', '--test', action='store_true',  help=f"Run the script using the test file ({TEST_FILE})")
+    parser.add_argument('-v', '--verbose', action='store_true', help="Enable verbose output.")
+    return parser.parse_args()
+
+def find_numbers(text):
+    return [int(n) for n in re.findall(r'-?\d+', text)]
+
+def parse_input(file_name) -> list[int]:
+    with open(file_name, 'r') as f:
+        data: list[int] = list(map(lambda s: find_numbers(s)[0], f))
+    return data
+
+# --- SOLVE ---
+def solve_part1(frequencies: list[int]) -> int:
+    """Solution for Part 1."""
+    return sum(frequencies)
+
+def solve_part2(frequencies: list[int]) -> int:
+    """Solution for Part 2."""
+    numbers: set[int] = set()
+    total: int = 0
+    for frequency in cycle(frequencies):
+        if total in numbers:
+            return total
+        numbers.add(total)
+        total += frequency
+
+if __name__ == '__main__':
+    args = get_args()
+    if args.test:
+        if not os.path.exists(TEST_FILE):
+            print(f"ERROR: Test file '{TEST_FILE}' not found.")
+            exit(1)
+        use_file = TEST_FILE
+    else:
+        use_file = INPUT_FILE
+    VERBOSE = args.verbose
+    
+    # Parsing
+    start_time = time.time()
+    data = parse_input(use_file)
+    log(f"Input parsed in {time.time()-start_time:.4f}s")
+    
+    # Part 1
+    start_time = time.time()
+    sol1 = solve_part1(data)
+    log(f"Part 1: {sol1}, took {time.time()-start_time:.4f}s")
+    
+    # Part 2
+    start_time = time.time()
+    sol2 = solve_part2(data)
+    log(f"Part 2: {sol2}, took {time.time()-start_time:.4f}s")
+
+    # --- PRINT SOLUTIONS ---
+    print(f"AOC_SOL_1={sol1}")
+    print(f"AOC_SOL_2={sol2}")
